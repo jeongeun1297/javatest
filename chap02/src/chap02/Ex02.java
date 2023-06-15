78c42951e01ba3102606f3948beca1006a60c006
Zzanggaring <limja77770@navercom>
2023-06-15 13:02:24 +0900

    1302

---
+++ chap02/src/chap02/Ex02.java
@@ -0,0 +1,19 @@
+package chap02;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+//		int x = 0b1011;
+//		int y = 0B10111;
+//		System.out.println(x);
+////		System.out.println(y);
+//		int x=013;
+//		int y=0206;
+//		System.out.println(x);
+		System.out.println(9);
+		int x=0xB3;
+		int y=0x2A0F;
+		System.out.println(x+y);
+	}
+
+}
