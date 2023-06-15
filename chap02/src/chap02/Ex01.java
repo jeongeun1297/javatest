687f2d0dbbdbaa9a0024d06b095fbbab026ccd38
Zzanggaring <limja77770@navercom>
2023-06-15 12:37:29 +0900

    test1237

---
+++ chap02/src/chap02/Ex01.java
@@ -0,0 +1,16 @@
+package chap02;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		byte a=127;
+		System.out.println(a);
+		short b= 32767;
+		System.out.println(b);
+		char c=97;
+		System.out.println(c);
+		int d=1000000000;
+		System.out.println(d);
+		long e=1000000000000000000L;
+		System.out.println(e);
+	}
+}
